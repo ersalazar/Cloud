c48607a329241e0bb45659b7bc150840b88f392b
ersalazar <71133367+ersalazar@users.noreply.github.com>
2022-03-02 08:49:40 -0600

    Update alumnos.sql

--- alumnos.sql
+++ alumnos.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
--- Versión del servidor:         10.7.1-MariaDB-1:10.7.1+maria~focal - mariadb.org binary distribution
--- SO del servidor:              debian-linux-gnu
+-- Versión del servidor:         8.0.27 - MySQL Community Server - GPL
+-- SO del servidor:              Win64
 -- HeidiSQL Versión:             11.3.0.6295
 -- --------------------------------------------------------
 
@@ -12,15 +12,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Volcando datos para la tabla ejemplo.alumnos: ~5 rows (aproximadamente)
-/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
-INSERT INTO `alumnos` (`nombre`, `apellido`, `mascota`, `id`) VALUES
-	('MAU', 'RODRIGUEZ', 'PAJARO', 1),
-	('ASAF', 'CONTREAS', 'PERRO', 2),
-	('RUBEN', 'GAMBOA', 'GATO', 3),
-	('ROBERTO', 'GAMBOA', 'IGUANA', 4),
-	('JUAN', 'FRNACO', 'TORTUGAS', 5);
-/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
+-- Volcando datos para la tabla parcial1.usuario: ~0 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
